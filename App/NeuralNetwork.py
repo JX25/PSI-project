00c3cf833a8c506059913eda6ead32ff8e5a4973
JX25 <jeehox25@gmail.com>
2018-12-30 16:29:51 +0100

    Normalize values

--- App/NeuralNetwork.py
+++ App/NeuralNetwork.py
@@ -1,14 +1,59 @@
 import tensorflow as tf
 
-# network parameters
-n_input = 13
-n_output = 1
-n_hidden_1 = 10
-n_hidden_2 = 10
-# learning parameters
-learning_const = 0.0001
-number_epochs = 1000
-batch_size = 100
 
-X = tf.placeholder("float", n_input)
-Y = tf.placeholder("float", n_output)
+class ANN:
+    # network parameters
+    def __init__(self):
+        self.n_input = 13
+        self.n_output = 1
+        self.n_hidden_1 = 10
+        self.n_hidden_2 = 10
+        # learning parameters
+        self.learning_const = 0.0001
+        self.number_epochs = 1000
+        self.batch_size = 100
+
+        self.X = tf.placeholder("float", self.n_input)
+        self.Y = tf.placeholder("float", self.n_output)
+
+        #biases for hidden1, hidden2, output
+        self.b1 = tf.Variable(tf.random_normal([self.n_hidden_1]))
+        self.b2 = tf.Variable(tf.random_normal([self.n_hidden_2]))
+        self.b3 = tf.Variable(tf.random_normal([self.n_output]))
+
+        #weights connecting between layers
+        self.w1 = tf.Variable(tf.random_normal([self.n_input, self.n_hidden_1]))
+        self.w2 = tf.Variable(tf.random_normal([self.n_hidden_1, self.n_hidden_2]))
+        self.w3 = tf.Variable(tf.random_normal([self.n_hidden_2, self.n_output]))
+
+    def multilayer_perceptron(self, input_data): #model
+        self.layer_1 = tf.nn.tanh(tf.add(tf.matmul(input_data, self.w1), self.b1))
+        self.layer_2 = tf.nn.tanh(tf.add(tf.matmul(self.layer_1, self.w2), self.b2))
+        self.out_layer = tf.add(tf.matmul(self.layer_2, self.w3), self.b3)
+        return self.out_layer
+
+
+def training(neural_network : ANN, Y, learn_const, batch_x, batch_y):
+    #loss of the error
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=neural_network, labels=Y))
+    #fix it
+    optimizer = tf.train.GradientDescentOptimizer(learn_const).minimize(loss_op)
+
+    init = tf.global_variables_initializer()
+    #create session
+    with tf.Session() as sess:
+        sess.run(init)
+        #training epoch
+        for epoch in range(neural_network.number_epochs):
+            sess.run(optimizer, feed_dict={neural_network.X: batch_x, neural_network.Y: batch_y})
+            if epoch % 100 == 0:
+                print("Epoch:", '%d' % (epoch))
+
+    return neural_network
+
+def test_model(neural_network : ANN, input, output):
+    pred = tf.nn.softmax(neural_network)
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(output, 1))
+    # accuracy
+    accuracy = tf.reduce_mean( tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({neural_network.X: input, neural_network.Y: output}))
--- App/main.py
+++ App/main.py
@@ -1,37 +1,87 @@
-# Ignore the seaborn warnings.
 import warnings
-from pprint import pprint
+import pandas as pd
+import math
 warnings.filterwarnings("ignore");
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy.stats
 
-# kandydat dla ktorego bedzie tworzona siec
+# Candidate prediction
+# ------------------------
 candidate = "Ted Cruz"
-# import danych z pliku .csv
+# ------------------------
+# import from .csv files
 data = pd.read_csv('data/primary_results.csv')
 candidate = data.loc[data.candidate == candidate]
 details = pd.read_csv('data/county_facts.csv')
-# uzywane kategorie
-facts_dictionary = ["area_name", "state", "PST045214", "SEX255214", "POP645213", "EDU635213", "EDU685213", "VET605213",
-                    "HSG445213", "INC910213", "SBO001207", "SBO315207", "SBO215207", "SBO015207", "LND110210"]
-columns = [col for col in details.columns if col in facts_dictionary]
-columns.append('state')
-details = details[columns]
-
+# used details
+details = details[['area_name', 'state_abbreviation', 'area_name', 'PST045214', 'SEX255214', 'POP645213', 'EDU635213',
+                   'EDU685213', 'VET605213', 'HSG445213', 'INC910213', 'SBO001207', 'SBO315207', 'SBO215207',
+                   'SBO015207',
+                   'LND110210']]
 # candidat_info : ( state, county, votes)
 candidat_info = []
-for state, county, votes in zip(candidate.state, candidate.county, candidate.fraction_votes):
+for state, county, votes in zip(candidate.state, candidate.county, candidate.votes):
     candidat_info.append((state, county, votes))
 # county detail : ( state, name, edu, edu, hsg, inc, lnd, pop, pst, vet, sex, sbo, sbo, sbo, sbo )
-county_details = []
-for ar, st, edu1, edu2, hsg, inc, lnd, pop, pst, vet, sex, sbo1, sbo2, sbo3, sbo4 in zip(details.area_name,
-    details.state, details.EDU635213, details.EDU685213, details.HSG445213, details.INC910213, details.LND110210,
-    details.POP645213, details.PST045214, details.VET605213, details.SEX255214, details.SBO001207, details.SBO315207,
-    details.SBO215207, details.SBO015207):
-    county_details.append(ar, st, edu1, edu2, hsg, inc, lnd, pop, pst, vet, sex, sbo1, sbo2, sbo3, sbo4)
-print(len(candidate))
+stats = []
+for (i, st, edu1, edu2, hsg, inc, lnd, pop, pst, vet, sex, sbo1, sbo2, sbo3, sbo4) in zip(
+        details.area_name.iterrows(),
+        details.state_abbreviation,
+        details.EDU635213, details.EDU685213, details.HSG445213, details.INC910213,
+        details.LND110210, details.POP645213, details.PST045214, details.VET605213, details.SEX255214,
+        details.SBO001207,
+        details.SBO315207, details.SBO215207, details.SBO015207):
+    stats.append((str.split(str(i[1]), '\n')[0].replace('area_name ', '').lstrip(),
+                  st, edu1, edu2, hsg, inc, lnd, pop, pst, vet, sex, sbo1, sbo2, sbo3, sbo4))
 
-# import wynikow
+# prepare test set
+# county = []
+states = ["California", "Texas", "Florida", "Iowa", "New Jersey", "Virginia"]
+# get votes from state
+votes = []
+for state in states:
+    sum_state = 0
+    for result in candidat_info:
+        if result[0] == state:
+            sum_state = sum_state + result[2]
+    votes.append(sum_state)
+
+# get state facts
+facts = []
+for state in states:
+    for stat in stats:
+        if state == stat[0] and math.isnan(stat[1]):
+            det = stat[2:]
+            facts.append(det)
+
+# to predict
+states_predict = ["Georgia", "Ohio", "New York"]
+# get votes from state to predict
+votes_to_predict = []
+for state in states_predict:
+    sum_state = 0
+    for result in candidat_info:
+        if result[0] == state:
+            sum_state = sum_state + result[2]
+        votes_to_predict.append(sum_state)
+# get state facts to predict
+facts_vtp = []
+for state in states_predict:
+    for stat in stats:
+        if state == stat[0] and math.isnan(stat[1]):
+            det = stat[2:]
+            facts_vtp.append(det)
+# convert to numpy array training set
+votes = np.asanyarray(votes)
+facts = np.asanyarray(facts)
+# convert to numpy array to predict
+votes_to_predict = np.asarray(votes_to_predict)
+facts_vtp = np.asanyarray(facts_vtp)
+# normalize values 0..1
+# normalize votes
+max_vote = np.max(votes)
+if np.max(votes_to_predict) > max_vote:
+    max_vote = np.max(votes_to_predict)
+votes = votes / max_vote
+votes_to_predict = votes_to_predict / max_vote
+# normalize facts
+print("XD")
